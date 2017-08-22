1e32dd36e751f2e21c0460616b983699a440ae85
adrianomcr <31012215+adrianomcr@users.noreply.github.com>
2017-08-22 13:01:28 -0300

    Add files via upload
    These are the necessary files to create a graph structure from the pixels files

---
+++ Matlab_create_structure/WriteListOfTuples.m
@@ -0,0 +1,25 @@
+
+
+
+%This script builds a list of tuples to be solved by the
+%Chinese-Postman-Problem
+
+
+
+
+load('Graph_data_36_meters.mat','graph')
+
+
+M = [];
+for k = 1:1:length(graph.Pol_coefs)
+    M(k,1) = graph.Pol_coefs(k).from;
+    M(k,2) = graph.Pol_coefs(k).to;
+    M(k,3) = graph.edge_matrix(M(k,1),M(k,2));
+end
+
+
+fprintf('list_edges = [')
+for k = 1:1:(length(M(:,1))-1)
+    fprintf('(%d, %d, %.4f),',M(k,1),M(k,2),M(k,3))
+end
+fprintf('(%d, %d, %.4f)]\n',M(end,1),M(end,2),M(end,3))
