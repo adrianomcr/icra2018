214d4e81bd7293c740d4bcffe67bc2564999355e
reza <rezajavanmard64@gmail.com>
2017-08-24 01:11:16 +0430

    preparing the file, using written function in other folders

--- distributed/scripts/replanning.py
+++ distributed/scripts/replanning.py
@@ -2,7 +2,9 @@
 # after solving MILP of the graph, we apply MST and CPP
 # the out put is a structure of the routes
 #from distributed.MST.MST_test import read_graph
-from MST_test import read_graph
+from MST_test import *
+from main import *
+
 
 
 # reading the graph
@@ -11,8 +13,11 @@ read_graph()
 #-------------------------------------
 # MILP Solver on input graph
 
+
 #-------------------------------------
 # MST
+execute_MST()
 
 #-------------------------------------
 # CPP
+main()
